97b2f84abd12834520e3ea2572aa730a1c9fe812
Your Name <you@example.com>
2023-05-13 16:05:40 +0000

    sonar - state - check - PROD - OK

--- src/main.js
+++ src/main.js
@@ -85,31 +85,5 @@ app.listen(3080, () => {
   logger.info(`/healthcheck on port 3080`)
 })
 
-var unusedVariable = 42;
-
-function multiply(a, b) {
-  return a * b;
-}
-
-multiply(2); // Missing the second argument
-
-try {
-  // Some code that may throw an exception
-} catch (error) {
-  // Empty catch block
-}
-
-var regex = /^(a+)+$/;
-
-var regex2 = /^(a+)+$/;
-try {
-  // Some code that may throw an exception
-} catch (error) {
-  // Empty catch block
-}
-function multiplyx(a, b) {
-  return a * b;
-}
-
 
 
